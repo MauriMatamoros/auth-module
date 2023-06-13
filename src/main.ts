7dd7261522b2323941e52f838cfcf123ebd19517
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2023-06-13 15:49:40 -0600

    :tada: initial commit

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const PORT = process.env.PORT || 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+    defaultVersion: '1',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth Module')
+    .setDescription('A module for authentication and authorization')
+    .setVersion('1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
+}
+bootstrap().then(() => {
+  console.log(`Running on port: ${PORT}`);
+});
